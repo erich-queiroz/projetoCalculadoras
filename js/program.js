3e239c802b22e37c13d01b4bc19ac56799ff1ceb
Erich Queiroz <95364468+erich-queiroz@users.noreply.github.com>
2023-02-14 10:16:37 -0300

    Adicionando arquivos iniciais HTML, CSS, JS
    Nesta versão o HTML já está estruturado e estilizado, além da sua programação implementada. Ainda não está responsivo

---
+++ js/program.js
@@ -0,0 +1,40 @@
+const initialAmountInput = document.querySelector("#initialAmount");
+const interestRateInput = document.querySelector("#interestRate");
+const timePeriodInput = document.querySelector("#timePeriod");
+const calculateButton = document.querySelector("#calculate");
+const infoButton = document.querySelector("#info");
+const resultParagraph = document.querySelector("#result");
+const infoSection = document.querySelector("#infoSection");
+
+
+
+calculateButton.addEventListener("click", function () {
+  if (!initialAmountInput.value || !interestRateInput.value || !timePeriodInput.value) {
+    initialAmountInput.style.border = "1px solid red";
+    interestRateInput.style.border = "1px solid red";
+    timePeriodInput.style.border = "1px solid red";
+    resultParagraph.innerHTML = "Preencha todos os campos!";
+    resultParagraph.style.color = "red";
+  } else {
+    resultParagraph.style.color = "black";
+    initialAmountInput.style.border = "1px solid lightgray";
+    interestRateInput.style.border = "1px solid lightgray";
+    timePeriodInput.style.border = "1px solid lightgray";
+    const initialAmount = parseFloat(initialAmountInput.value);
+    const interestRate = parseFloat(interestRateInput.value);
+    const timePeriod = parseFloat(timePeriodInput.value);
+    const result = initialAmount * (1 + (interestRate / 100)) ** timePeriod;
+    resultParagraph.innerHTML = `O montante final é de R$ ${result.toFixed(2)}`;
+  }
+});
+
+
+infoButton.addEventListener("click", function () {
+  infoSection.style.display = "block";
+});
+
+
+
+infoSection.addEventListener("click", function () {
+  infoSection.style.display = "none";
+});
